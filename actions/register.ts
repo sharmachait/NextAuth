db1ee0f3cd2dd02c19bb8ad1aa7dc7c56f473375
sharmachait <chait8126@gmail.com>
2024-06-24 23:39:08 +0530

    next auth

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,29 @@
 'use server';
 import * as z from 'zod';
-import { LoginSchema, RegisterSchema } from '@/schemas';
+import { RegisterSchema } from '@/schemas';
+import bcrypt from 'bcrypt';
+import _dbContext from '@/lib/dbContext';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  const validatedFields = LoginSchema.safeParse(values);
-  console.log('hello');
+  const validatedFields = RegisterSchema.safeParse(values);
+
   if (!validatedFields.success) {
     return { error: 'Invalid fields!' };
   }
-  return { success: 'Email sent!' };
+
+  const { email, password, name } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: 'An account with this email already exists!' };
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await _dbContext.user.create({
+    data: { email: email, password: hashedPassword, name: name },
+  });
+
+  //TODO: send verification email token
+
+  return { success: 'Email sent, please verify your account!' };
 };
